5a0460e43a20451f2f465512c2a6bb4e7c20a0c3
Maurício Szabo <mauricio.szabo@gmail.com>
2015-10-26 23:40:48 -0200

    Corrected lots of code.

--- src/router/core.clj
+++ src/router/core.clj
@@ -1,9 +1,11 @@
 (ns router.core
-;   (:require [castra.middleware :as castra]
-;             [compojure.core :as c]
-;             [compojure.route :as route]
-;             [ring.middleware.defaults :as d]
-;             [ring.util.response :as response]))
+  (:require [castra.middleware :as castra]
+            [compojure.core :as c]
+            [compojure.route :as route]
+            [ring.middleware.defaults :as d]
+            [compojure.core :refer :all]
+            [compojure.route :as route]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 ;
 ; (c/defroutes app-routes
 ;   (c/GET "/" req (response/content-type (response/resource-response "index.html") "text/html"))
@@ -17,16 +19,20 @@
 ;       (d/wrap-defaults d/api-defaults)))
 ;
 ; (ns users-systems.handler
-  (:require [compojure.core :refer :all]
-            [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
 (use 'selmer.parser)
 
 (defroutes app-routes
-  (GET "/" [] (render-file "people.html" {}))
+  (GET "/" [] (render-file "people.html"
+    {:users [{:id 10, :name "foo"}]}))
 
   (route/not-found "Not Found"))
 
-(def app
-  (wrap-defaults app-routes site-defaults))
+; (def app
+;   (wrap-defaults app-routes site-defaults))
+
+(def router
+  (-> app-routes
+      (castra/wrap-castra 'app.api)
+      (castra/wrap-castra-session "a 16-byte secret")
+      (d/wrap-defaults d/api-defaults)))
